6cdbebc496ce29cb238fdc1b8c1ce751112358c8
Pablo Leonrodenas <pablo.leonrodenas@england.nhs.uk>
2023-01-26 17:04:15 +0000

    Added Readme file with sources used in this project and process to import .xlsx files into R

--- 03 Import Excel data into R.R
+++ 03 Import Excel data into R.R
@@ -1,1 +1,17 @@
-# 03 Import data into Excel
+# 03 Import Excel data into R
+
+# Load required packages at once (readxl,here,dplyr,janitor)
+pacman::p_load(readxl,here,dplyr,janitor)
+
+Excel_file <-list.files (path = "./data" ,pattern = "xlsx$")
+Excel_file
+
+# [1] "RTT_TS_data.xlsx"
+Excel_tabs <- excel_sheets("./data/RTT_TS_data.xlsx")
+Excel_tabs
+
+# [1] "Full Time Series"
+
+# Import data using readxl and janitor
+# read_excel() parameters
+# sheet = n . Define on which sheet to start the reading of data
