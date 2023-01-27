eef1d8b07e47cd52ce02877c70efd4ee9146116f
Pablo Leonrodenas <pablo.leonrodenas@england.nhs.uk>
2023-01-27 16:21:35 +0000

    Updated comment

--- 05 Tidy up downloaded AE data.R
+++ 05 Tidy up downloaded AE data.R
@@ -1,6 +1,6 @@
 # 05 Tidy up downloaded AE data
 
-# 1. Load required packages
+# 1. Load required packages 
 pacman::p_load(readxl,here,dplyr,janitor)
 
 # Check existing files on data folder 
@@ -24,7 +24,7 @@ here()
 AE_tabs <- excel_sheets(here("data","AE_England_data.xls"))
 AE_tabs
 
-[1] "Activity"    "Performance" "Chart Data"  "Charts"  
+# [1] "Activity"    "Performance" 
 
 # We read in data from Excel using READXL package
 # From "readxl" package we use the read_excel function to read in data from Excel file
@@ -39,11 +39,66 @@ AE_tabs
 
 # Also we make use of the clean_names() from janitor package to obtain clear variable names
 
+
+# 2.2 Skip first rows of data containing Notes and file description information in the original .xls file
+
+# We start now importing the data by cleaning out the redundant text rows 
+# from the original input .xls file 
+#-	We want to import data from the First sheet “Activity”/ Sheet =1 
+#-	We also need to skip 17 rows to obtain the right column variable values
+#-	We need to bear in mind that the first 4 rows are A&E Attendances and the from row 5 on wards are A&E Admissions
+
+
 # Skip certain rows of data: 
 AE_data<- read_excel(
-                     here("data", "CCG_1.17_I01968_D.xlsx"), 
-                     sheet = 3, skip =13) %>% 
+                     here("data", "AE_England_data.xls"), 
+                     sheet = 1, skip =17) %>% 
                      clean_names()
-# How to select rows of data 
-Tab10202in <- read_excel(here("Input_files",DataA),sheet = 1,range = "C10:F18",skip = 1,na = "")
+AE_data
 
+names(AE_data)
+
+# Example: How to select rows of data 
+# Import only "Type 1 Departments- Major A&E" A&E Attendances data into R
+
+AE_data_Type1_ATT <- read_excel(here("data","AE_England_data.xls"),
+                                sheet = 1,skip =17, range = "C18:D123",na = "")
+AE_data_Type1_ATT
+
+# 3. Subset original imported AE_data set to Keep A&E Attendances
+
+
+# From file  AE_England_data.xls
+AE_data_subset<- read_excel(
+                      here("data", "AE_England_data.xls"), 
+                      sheet = 1, skip =17) %>% 
+                      clean_names() %>% 
+                      select(
+                        "x1",                                                                      
+                        "period",                                                                  
+                        "type_1_departments_major_a_e",                                            
+                        "type_2_departments_single_specialty",                                     
+                        "type_3_departments_other_a_e_minor_injury_unit",                          
+                        "total_attendances" 
+                      )
+AE_data_subset
+
+# 4. Rename variables in preparation for creating a ggplot2 plot
+
+# 4.1 First we remove X1 extra variable
+AE_data_plot <- AE_data_subset %>% 
+                select(-x1)
+AE_data_plot
+
+# 4.2 THen we rename remainig variables to shorten their names
+
+AE_plot_prep <- AE_data_plot %>% 
+                select(
+                  period,
+                  type_1_Major_att = type_1_departments_major_a_e,
+                  type_2_Single_esp_att = type_2_departments_single_specialty,
+                  type_3_other_att = type_3_departments_other_a_e_minor_injury_unit,
+                  total_att = total_attendances
+                  
+                ) 
+AE_plot_prep
