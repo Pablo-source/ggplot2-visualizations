513822b76c6ef56fc5f1b15987a065f0440c3b39
Pablo Leonrodenas <pablo.leonrodenas@england.nhs.uk>
2023-01-27 15:46:19 +0000

    Downloaded AE data as xls from URL

--- 04 AandE Excel data into R from URL.R
+++ 04 AandE Excel data into R from URL.R
@@ -1,10 +1,10 @@
-# 04 Import A&E Excel data into R from URL
+# 04 Import A&E Excel data into R from URL 
 
 #Searching for the href HTML tag we can find the corresponding .xls file to import it into R
 #<p><a href="https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2019/11/Timeseries-monthly-Unadjusted-9kidr.xls">Unadjusted: Monthly A&amp;E Time series April 2019 (XLS, 364K)</a><br />
-  
-  
 
+pacman::p_load(readxl,here,dplyr,janitor)
+  
 AE_data <- function() {
   
   if(!dir.exists("data")){dir.create("data")}
@@ -13,14 +13,14 @@ AE_data <- function() {
   # Download Excel file to a Project sub-folder called "data"
   # Created previously using an adhoc project structure function
   
-  xlsFile = "RTT_TS_data.xlsx"
+  xlsFile = "AE_England_data.xlsx"
   
   download.file(
-    url = 'https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2023/01/RTT-Overview-Timeseries-Including-Estimates-for-Missing-Trusts-Nov22-XLS-98K-63230.xlsx',
+    url = 'https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2019/11/Timeseries-monthly-Unadjusted-9kidr.xls',
     destfile = here("data",xlsFile),
     mode ="wb"
   )
   
 }
-# Download RTT data function (no arguments)
-RTT_TS_data()
+# Download A&E data function (no arguments)
+AE_data()
---
+++ 05 Tidy up downloaded AE data.R
@@ -0,0 +1,4 @@
+# 05 Tidy up downloaded AE data
+
+# 1. Load required packages
+pacman::p_load(readxl,here,dplyr,janitor)
