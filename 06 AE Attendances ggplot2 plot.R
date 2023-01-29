06d92edaa535ed82d09e5e6c9b17bb933853997f
Pablo Leon <datavizvalencia@gmail.com>
2023-01-29 17:14:24 +0000

    Added new plots to showcase themes

--- 06 AE Attendances ggplot2 plot.R
+++ 06 AE Attendances ggplot2 plot.R
@@ -1,6 +1,6 @@
 # 06 AE Attendances ggplot2 plot.R
 
-# We start by Loading previous data set
+# We start by Loading previous data set 
 # AEATT_plot 
 names(AEATT_plot)
 head(AEATT_plot)
@@ -19,6 +19,10 @@ names(AEATT_plot)
 #[1] "period"                "type_1_Major_att"      "type_2_Single_esp_att"
 #[4] "type_3_other_att"      "total_att" 
 
+
+# We include geom_line() to display an initial plot
+# geom_line()
+
 TypeI_att_plot <- AEATT_plot %>% 
                   select(period, type_1_Major_att) %>% 
                   ggplot(aes(x = period, y = type_1_Major_att )) +
@@ -36,6 +40,11 @@ TypeI_att_plot
 # https://github.com/nhs-r-community/NHSRtheme
 # https://nhsengland.github.io/nhs-r-reporting/documentation/nhs-colours.html
 
+# We include new parameters in geom_line() geom 
+# geom_line()
+# 1. Include colour geom_line(color = "#0072CE")
+# 2. Include line size (1)
+# 3. Include linetype (linetype="dashed", linetype="dotted", 0  is for “blank”, 1 is for “solid”, 2 is for “dashed”)
 # NHS Blue	#005EB8
 # NHS Bright Blue	#0072CE 
 TypeI_att_plot <- AEATT_plot %>% 
@@ -74,3 +83,126 @@ TypeI_dot_line_plot <- AEATT_plot %>%
 TypeI_dot_line_plot
 
 ggsave("plots/A&E_Attendances_Type_1_England_line_point_geom.png", width = 6, height = 4)
+
+# 4. Modify X and Y axis labels
+Axis_labels_plot <- AEATT_plot %>% 
+                          select(period, type_1_Major_att) %>% 
+                          ggplot(aes(x = period, y = type_1_Major_att)) +
+                          geom_line(color="#0072CE", size=1,  linetype=1) +
+                          # Included new geom
+                          geom_point(fill="#00A9CE",shape=21,show.legend = FALSE) +
+                          labs(title = "A&E Attendances in England: Type 1 Departments - Major A&E",
+                               subtitle ="Source: https://www.england.nhs.uk/statistics/statistical-work-areas/ae-waiting-times-and-activity/",
+                               # Change X and Y axis labels
+                               x = "Period", 
+                               y = "Type I Attendances") 
+                          
+Axis_labels_plot
+
+ggsave("plots/A&E_Attendances_Type_1_England_aixs_titles.png", width = 6, height = 4)
+
+# 5. Remove tick marks 
+# theme(axis.ticks = element_blank())
+
+Axis_ticks_removed <- AEATT_plot %>% 
+  select(period, type_1_Major_att) %>% 
+  ggplot(aes(x = period, y = type_1_Major_att)) +
+  geom_line(color="#0072CE", size=1,  linetype=1) +
+  # Included new geom
+  geom_point(fill="#00A9CE",shape=21,show.legend = FALSE) +
+  labs(title = "A&E Attendances in England: Type 1 Departments - Major A&E",
+       subtitle ="Source: https://www.england.nhs.uk/statistics/statistical-work-areas/ae-waiting-times-and-activity/",
+       # Change X and Y axis labels
+       x = "Period", 
+       y = "Type I Attendances") +
+  theme (axis.ticks = element_blank())
+
+Axis_ticks_removed
+
+ggsave("plots/A&E_Attendances_Type_1_England_Axis_ticks_removed.png", width = 6, height = 4)
+
+# 6. Include theme 
+# Arrange different plots using gridExtra
+library(gridExtra)
+
+# Theme bw
+# theme_bw()
+TypeI_theme_bw  <- AEATT_plot %>% 
+                      select(period, type_1_Major_att) %>% 
+                      ggplot(aes(x = period, y = type_1_Major_att)) +
+                      geom_line(color="#0072CE", size=1,  linetype=1) +
+                      # Included new geom
+                      geom_point(fill="#00A9CE",shape=21,show.legend = FALSE) +
+                      labs(title = "A&E Attendances \n Theme_bw",
+                           subtitle ="Source: https://www.england.nhs.uk/statistics/statistical-work-areas/ae-waiting-times-and-activity/",
+                           # Change X and Y axis labels
+                           x = "Period", 
+                           y = "Type I Attendances") +
+                      theme (axis.ticks = element_blank()) +
+                      theme_bw()
+
+TypeI_theme_bw
+
+# Theme light
+# theme_light()
+
+TypeI_theme_light  <- AEATT_plot %>% 
+  select(period, type_1_Major_att) %>% 
+  ggplot(aes(x = period, y = type_1_Major_att)) +
+  geom_line(color="#0072CE", size=1,  linetype=1) +
+  # Included new geom
+  geom_point(fill="#00A9CE",shape=21,show.legend = FALSE) +
+  labs(title = "A&E Attendances \n Theme_light",
+       subtitle ="Source: https://www.england.nhs.uk/statistics/statistical-work-areas/ae-waiting-times-and-activity/",
+       # Change X and Y axis labels
+       x = "Period", 
+       y = "Type I Attendances") +
+  theme (axis.ticks = element_blank()) +
+  theme_light()
+
+TypeI_theme_light
+
+# Theme classic
+# theme_classic()
+
+TypeI_theme_classic  <- AEATT_plot %>% 
+  select(period, type_1_Major_att) %>% 
+  ggplot(aes(x = period, y = type_1_Major_att)) +
+  geom_line(color="#0072CE", size=1,  linetype=1) +
+  # Included new geom
+  geom_point(fill="#00A9CE",shape=21,show.legend = FALSE) +
+  labs(title = "A&E Attendances \n Theme_classic",
+       subtitle ="Source: https://www.england.nhs.uk/statistics/statistical-work-areas/ae-waiting-times-and-activity/",
+       # Change X and Y axis labels
+       x = "Period", 
+       y = "Type I Attendances") +
+  theme (axis.ticks = element_blank()) +
+  theme_classic()
+
+TypeI_theme_classic
+
+# Theme dark 
+# theme_dark() 
+
+TypeI_theme_dark  <- AEATT_plot %>% 
+  select(period, type_1_Major_att) %>% 
+  ggplot(aes(x = period, y = type_1_Major_att)) +
+  geom_line(color="#0072CE", size=1,  linetype=1) +
+  # Included new geom
+  geom_point(fill="#00A9CE",shape=21,show.legend = FALSE) +
+  labs(title = "A&E Attendances, \n Theme_dark",
+       subtitle ="Source: https://www.england.nhs.uk/statistics/statistical-work-areas/ae-waiting-times-and-activity/",
+       # Change X and Y axis labels
+       x = "Period", 
+       y = "Type I Attendances") +
+  theme (axis.ticks = element_blank()) +
+  theme_dark()
+
+TypeI_theme_dark
+
+# Arrange all plots into a single image using grid-arrange()
+GRID_PLOT <- grid.arrange(TypeI_theme_bw,TypeI_theme_light,
+             TypeI_theme_classic,TypeI_theme_dark,
+             ncol=4)
+
+ggsave("plots/A&E_Attendances_Grid_Arrange_themes_gallery.png", width = 6, height = 4)
