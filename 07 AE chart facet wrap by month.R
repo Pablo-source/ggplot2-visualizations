b7e50e11dd14442979ffbd75746581d795c01a36
Pablo Leonrodenas <pablo.leonrodenas@england.nhs.uk>
2023-02-01 15:30:31 +0000

    Added plots

--- 07 AE chart facet wrap by month.R
+++ 07 AE chart facet wrap by month.R
@@ -158,7 +158,34 @@ AE_att_wrap_year
 
 ggsave("plots/11_AE_Attendances_facet_wrap.png", width = 6, height = 4) 
 
+# Apply format to title and subtitles in facet_wrap plots
 
 
+AE_att_wrap_formatted   <- Att_facet %>% 
+  select(type_1_Major_att,Year,Monthf) %>% 
+  ggplot(aes(x = Monthf, y = type_1_Major_att,group = Year)) +
+  geom_line(color="#0072CE", size=1,  linetype=1) +
+  facet_wrap(~ Year) +
+  labs(title = "A&E Attendances in England: Type 1 Departments - Major A&E",
+       subtitle ="Type I attendances by month by year. 2011-2018",
+       # Change X and Y axis labels
+       x = "Period", 
+       y = "Type I Attendances") +
+  theme_light()  +
+
+theme(
+  axis.ticks = element_blank(),
+  # A value of “plot” means the titles/caption are aligned to the entire plot
+  # Apply format to title plot
+  plot.title.position = "plot", 
+  plot.title = element_text(margin = margin (b=10), colour = "dodgerblue2", face = "bold"), # Skyblue1 colour
+  # Apply format to sub-title
+  plot.subtitle = element_text(
+    size =8, colour = "deepskyblue2", face = "bold")
+) 
+
+AE_att_wrap_formatted
+
+ggsave("plots/12_AE_Attendances_facet_wrap.png", width = 6, height = 4) 
 
 
