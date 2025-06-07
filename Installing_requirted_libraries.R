39bcdc44a8ad2595d87dc7ea23d689a87959f0fb
Pablo-source <datavizvalencia@gmail.com>
2025-06-07 20:08:49 +0100

    Installing first packages on this fresh R installation in Ubuntu

--- Installing_requirted_libraries.R
+++ Installing_requirted_libraries.R
@@ -1,3 +1,13 @@
 # Installing required libraries on this fresh Ubuntu installation on RStudio
 # 07/06/2025
 here()
+# [1] "/home/pablo/Documents/Pablo_ubuntu/Repos/Pablo_source_repo/ggplot2-visualizations"
+
+install.packages("readxl",dependencies = TRUE)
+install.packages("tidyverse",dependencies = TRUE)
+install.packages("janitor",dependencies = TRUE)
+
+# Then Load required libraries 
+library(tidyverse)
+library(readxl)
+library(janitor)
